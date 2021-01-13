0a0f686fb2bb13a270ad281b1e97b425c038b4dd
Stanislau <omgnoooob@gmail.com>
2021-01-13 23:58:12 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (array == [""] || array == null || !array || array.length == 0) {
+    return 0;
+  } else {
+    return Math.min(...array);
+  }
 }
 
-exports.max = function max (array) {
-  return 0;
+
+
+exports.max = function max(array) {
+  if (array == [""] || array == null || !array || array.length == 0) {
+    return 0;
+  } else {
+    return Math.max(...array);
+  }
+};
+
+exports.avg = function avg(array) {
+  if (array == [""] || array == null || !array || array.length == 0) {
+    return 0;
+  } else {
+    let sum = 0;
+    array.forEach((element) => {
+      sum += element;
+      return sum;
+    });
+    return sum/array.length;
+  }
 }
 
-exports.avg = function avg (array) {
-  return 0;
-}
